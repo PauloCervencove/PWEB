427437efc7b085dae8e4322ca5b3f6e30ae8a540
PauloCervencove <62033278+PauloCervencove@users.noreply.github.com>
2022-11-23 00:48:53 -0300

    Add files via upload

---
+++ Atividade12/script.js
@@ -0,0 +1,14 @@
+var janela = document.getElementById("janela");
+
+
+function abrirJanela(){
+        janela.src = "imagens/janelaaberta.jpg";
+}
+
+function fecharJanela(){
+        janela.src = "imagens/janelafechada.jpg";
+}
+
+function quebrarJanela(){
+        janela.src = "imagens/janelaquebrada.jpg";
+}
